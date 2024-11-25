bb727dbbc6b1240ef64eff30c84d12dc4f0b53c2
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-11-25 09:50:51 +0200

    start

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export * from './Maine/Maine';
-export * from './BorderChanges/BorderChanges';
-export * from './BackgroundChanges/BackgroundChanges';
-export * from './WidthChanges/WidthChanges';
-export * from './HeightChanges/HeightChanges';
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import ReactDOM from 'react-dom/client';
-import {RouterProvider} from "react-router-dom";
 
 import './index.css'
-import {router} from "./router";
-import {ContextProvider} from "./hoc";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ContextProvider>
-        <RouterProvider router={router}/>
-    </ContextProvider>
+    <h1>React</h1>
 );
 
